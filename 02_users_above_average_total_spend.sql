d8056ae86169a302ce93e31144261f89c2653e51
Handenurseker <154757544+Handenurseker@users.noreply.github.com>
2026-03-15 01:27:14 +0300

    Update 02_users_above_average_total_spend.sql

--- 02_users_above_average_total_spend.sql
+++ 02_users_above_average_total_spend.sql
@@ -1,7 +1,19 @@
-SELECT  ROUND(AVG(O.TOTALPRICE),0) FROM ORDERS O
-HAVING AVG(O.TOTALPRICE)>
-  (
-  SELECT ROUND(SUM(O2.TOTALPRICE),0) TOPLAM_SATIS FROM USERS U
-  JOIN ORDERS O2 ON O2.USERID=U.ID
-  GROUP BY U.NAMESURNAME,U.ID
+-- =====================================================
+-- Query: Users Above Average Total Spend
+-- Description: This query identifies users whose total spending is above the average total spending
+--across all users.
+-- Tables Used: ORDERS, USERS
+-- =====================================================
+
+SELECT U.ID AS UserID, U.NAMESURNAME AS UserName, SUM(O.TOTALPRICE) AS TotalSpend 
+FROM USERS U
+JOIN ORDERS O ON U.ID=O.USERID
+GROUP BY U.ID, U.NAMESURNAME
+HAVING SUM(O.TOTALPRICE) > 
+  (SELECT AVG(T.TOTALPRICE) FROM 
+    (SELECT U2.ID AS UserID, SUM(O2.TOTALPRICE) AS TotalSpend 
+    FROM USERS U2
+    JOIN ORDERS O2 ON U2.ID=O2.USERID
+    GROUP BY U2.ID) AS T
   )
+ORDER BY TotalSpend DESC
