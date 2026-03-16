e38c0196b70ebc6f6dcc6cf47297d21222c5f96f
Handenurseker <154757544+Handenurseker@users.noreply.github.com>
2026-03-16 16:05:30 +0300

    Update 04_customers_lifecycle_analysis.sql

--- 04_customers_lifecycle_analysis.sql
+++ 04_customers_lifecycle_analysis.sql
@@ -1,6 +1,17 @@
 -- =====================================================
 -- Query: Customer Lifecycle Analysis
--- Description: This query identifies cities whose total sales are above the average total sales
---across all cities.
--- Tables Used: ORDERS, ADDRESS, CITIES
+-- Description: This query analyzes customer purchasing behavior by identifying each customer's first order date,
+-- last order date, and total number of orders
+-- Tables Used: ORDERS, USERS
 -- =====================================================
+
+SELECT 
+	U.ID AS UserID,
+	U.NAMESURNAME AS UserName,
+	MIN(O.DATE_) AS FirstOrderDate,
+	MAX(O.DATE_) AS LastOrderDate,
+	COUNT(DISTINCT O.ID) AS OrderCount
+FROM ORDERS O
+JOIN USERS U ON U.ID=O.USERID
+GROUP BY U.ID, U.NAMESURNAME
+ORDER BY U.ID
